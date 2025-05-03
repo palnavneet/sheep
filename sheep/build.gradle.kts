c896e0e2d437dc56f81994d6378a03b43e7f3de1
palnavneet <navneetpal.dev@gmail.com>
2025-05-03 23:00:05 +0530

    feat: Update build configurations for Java 11 compatibility
    This commit updates the build configurations to use Java 11 instead of Java 8 and 17, ensuring compatibility with newer Java versions. Key changes include:
    
    -   **jitpack.yml:**
        -   Changed the JDK version from `openjdk17` to `openjdk11`.
        - Removed unnecessary steps to install java 17.0.7-open
    -   **sheep/build.gradle.kts:**
        -   Set `sourceCompatibility` and `targetCompatibility` to `JavaVersion.VERSION_11`.
        -   Updated `jvmTarget` to `"11"`.
    -   **sheep/build.gradle.kts:**
        - Updated groupId in publication to com.github.palnavneet

--- sheep/build.gradle.kts
+++ sheep/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
@@ -47,7 +47,7 @@ dependencies {
 publishing{
     publications {
         register<MavenPublication>("release"){
-            groupId = "https://github.com/palnavneet"
+            groupId = "com.github.palnavneet"
             artifactId = "sheep"
             version = "1.0.0-alpha"
 
